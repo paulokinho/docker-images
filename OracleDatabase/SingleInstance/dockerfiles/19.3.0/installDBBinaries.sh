acbccf02715805572a6417ea7ee83612e2544c8f
Paulo Nascimento <paulokinho@terra.com.br>
2024-08-29 01:31:26 +0200

    Donator Working

--- OracleDatabase/SingleInstance/dockerfiles/19.3.0/installDBBinaries.sh
+++ OracleDatabase/SingleInstance/dockerfiles/19.3.0/installDBBinaries.sh
@@ -50,9 +50,6 @@ sed -i -e "s|###ORACLE_HOME###|$ORACLE_HOME|g" "$INSTALL_DIR"/"$INSTALL_RSP"
 
 # Install Oracle binaries
 cd "$ORACLE_HOME"       && \
-mv "$INSTALL_DIR"/"$INSTALL_FILE_1" "$ORACLE_HOME"/ && \
-unzip "$INSTALL_FILE_1" && \
-rm "$INSTALL_FILE_1"    && \
 "$ORACLE_HOME"/runInstaller -silent -force -waitforcompletion -responsefile "$INSTALL_DIR"/"$INSTALL_RSP" -ignorePrereqFailure && \
 cd "$HOME"
 
